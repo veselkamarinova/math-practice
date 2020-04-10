e64dc505b56c7bc752bb2c272368292be098c754
Veselka Marinova <vsl.marinova@gmail.com>
2020-04-10 15:51:29 +0300

    starting on the skeleton on the game class

---
+++ game.py
@@ -0,0 +1,15 @@
+from calculation.summation import Calculation
+
+
+class Game:
+    print("Здравей!Днес ще се забавляваме с математика. Как е твоето име?")
+    name = input()
+    print('Добре да се забавляваме, ' + name + '. Нека да започнем с действие събиране!')
+
+    @staticmethod
+    def run_game(self):
+        takeGuess = int(input("="))
+        for i in range(11):
+            pass
+
+
