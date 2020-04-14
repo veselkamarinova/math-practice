afa44e01ceb5d11df4eb7ed3ecf1e007e90ccbd0
Veselka Marinova <vsl.marinova@gmail.com>
2020-04-14 09:58:08 +0300

    change the variables names

--- game.py
+++ game.py
@@ -9,8 +9,8 @@ class Game:
 
         username = input("Здравей! Как се казваш?\n" "Моето име е: ")
         print(f"Добре да се забавляваме, {username}. Нека да започнем с действие събиране!")
-        sum_not_correct_answer = 0
-        sum_correct_answer = 0
+        incorrect_count = 0
+        correct_count = 0
 
         for i in range(1, 11):
             print(f"Въпрос номер {i}:")
@@ -20,33 +20,33 @@ class Game:
             while True:
                 if user_answer == str(answer):
                     print(f"Поздравления, твоят отговор {answer} е правилен.")
-                    sum_correct_answer += 1
+                    correct_count += 1
                     break
                 elif user_answer != str(answer):
                     print(f"Отговорът ти не е правилен. Опитай пак! На колко е равно:\n {question} ?")
                     user_answer = input("= ")
-                    sum_not_correct_answer += 1
+                    incorrect_count += 1
 
         print(f"Браво, {username} ти завърши математическата игра!")
-        if sum_correct_answer > sum_not_correct_answer:
-            if sum_not_correct_answer == 0:
+        if correct_count > incorrect_count:
+            if incorrect_count == 0:
                 print(
                     f"Отлична работа, всички отговори са правилни! Твоята награда е 30 минути бонус игра на телефона!")
-            elif sum_not_correct_answer < 3:
+            elif incorrect_count < 3:
                 print(
-                    f'Броят на неверните отговорие е: {sum_not_correct_answer}! '
+                    f'Броят на неверните отговорие е: {incorrect_count}! '
                     f'Получаваш награда от 20 минути бонус игра на телефона!'
                 )
-            elif 3 == sum_not_correct_answer < 5:
+            elif 3 == incorrect_count < 5:
                 print(
-                    f"Броят на неверните отговорие е: {sum_not_correct_answer}!"
+                    f"Броят на неверните отговорие е: {incorrect_count}!"
                     f" Твоята поущтрителна наградата е 15 минути бонус игра на телефона!"
                 )
             else:
                 print(f" Трябва да решаваш повече задачи, за да получиш награда!")
         else:
             print(
-                f"От 10 задачи ти имаш {sum_not_correct_answer} грешни отговора."
+                f"От 10 задачи ти имаш {incorrect_count} грешни отговора."
                 f" Трябва да решаваш повече задачи, за да получиш награда!")
 
 
